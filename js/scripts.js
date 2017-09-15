c5d5686536b3d124fb0282dec739a70545ebf19d
Michael Arrington <mike@Macrilicious-2.local>
2017-09-15 11:56:52 -0700

    Add all questions and answers with trackable names and IDs in hideable DIVs.

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  $("form#quiz").submit(function(event) {
+    var question1 = $("input:radio[name=question1]:checked").val();
+    var question2 = $("input:radio[name=question2]:checked").val();
+
+
+
+  event.preventDefault();
+  });
+});
+
+
+
+
+
+
+
+
+var question1 = $("input:radio[name=question1]:checked").val();
+var question2 = $("input:radio[name=question2]:checked").val();
+
+var = goTo(close,open) {
+  $("close").hide("slow");
+  $("open").show("slow");
+}
